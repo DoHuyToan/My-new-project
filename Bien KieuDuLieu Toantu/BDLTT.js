a12922e0f076649384efae471c99e94e25c9f575
DoHuyToan <dhtoan89@gmail.com>
2021-08-17 22:20:48 +0700

    add file

---
+++ Bien KieuDuLieu Toantu/BDLTT.js
@@ -0,0 +1,6 @@
+let Vatly = parseInt(prompt("Diem Vatly:"))
+let Hoahoc = parseInt(prompt("Diem Hoahoc:"))
+let Sinh = parseInt(prompt("Diem Sinh:"))
+
+let DiemTB = (Vatly + Hoahoc + Sinh)/3;
+document.write("Điểm TB là: " + DiemTB);
