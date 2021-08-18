13a8888f09bcde664f0a0b7158dda21e40eb230d
DoHuyToan <dhtoan89@gmail.com>
2021-08-18 10:36:08 +0700

    add file

--- Bien KieuDuLieu Toantu/UngDungMayTinh.js
+++ Bien KieuDuLieu Toantu/UngDungMayTinh.js
@@ -1,6 +1,6 @@
 function PhepCong(){
-    let number1= document.getElementById("number1").value
-    let number2=document.getElementById("number2").value
+    let number1= parseInt(document.getElementById("number1").value)
+    let number2= parseInt(document.getElementById("number2").value)
 let KetQua= number1 + number2
     document.getElementById("KetQua").innerHTML="Ket Qua La" + KetQua
 }
