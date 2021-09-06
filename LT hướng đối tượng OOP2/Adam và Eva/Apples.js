3698a05b2239a366afa705e1704c3d2c36db8395
DoHuyToan <dhtoan89@gmail.com>
2021-09-06 11:14:06 +0700

    Can thuc hanh them

---
+++ LT hướng đối tượng OOP2/Adam và Eva/Apples.js
@@ -0,0 +1,20 @@
+class Apple {
+    constructor() {
+        this.weight=10;
+        this.status=true;
+    }
+    getWeight(){
+        return this.weight;
+    }
+    decrease(){
+        if(this.status){
+            this.weight--
+        }
+    }
+    checkStatus() {
+        if (this.weight===0){
+            return !this.status
+        }
+    }
+}
+// let newApple = new Apple()
---
+++ LT hướng đối tượng OOP2/Adam và Eva/Human.js
@@ -0,0 +1,21 @@
+class Human {
+    constructor(_name,_gender,_weight) {
+        this.name= _name;
+        this.gender= _gender;
+        this.weight= _weight;
+    }
+    getWeight(){
+        return this.weight;
+    }
+    talk(text){
+        console(text);
+    }
+    check(apple) {
+            return apple.getWeight()>0;
+    }
+    eat(apple){
+        if(this.check(apple)){
+            this.weight++;
+            apple.decrease();
+        }
+    }
